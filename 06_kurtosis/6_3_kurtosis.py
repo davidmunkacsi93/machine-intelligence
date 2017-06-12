ae1c9daa1a3981684d43baef62a14c9dc2369b65
fabian247 <onlinezeug@gmx.net>
2017-06-12 22:32:52 +0200

    6.3 almost finished

--- 06_kurtosis/6_3_kurtosis.py
+++ 06_kurtosis/6_3_kurtosis.py
@@ -1,6 +1,12 @@
 import numpy as np
+import scipy as scipy
 import scipy.io as io
 from sklearn.decomposition import PCA
+from sklearn import preprocessing as pre
+import math
+import matplotlib.pyplot as plt
+import seaborn as sea
+import pandas as pd
 
 #load data from mat file
 distrib = io.loadmat("distrib.mat")
@@ -10,120 +16,100 @@ uniform = distrib.get("uniform")
 laplacian = distrib.get("laplacian")
 
 # a) apply mixing
-A = np.asarray([[4,3],[2,1]])
-xNormal = np.dot(A,normal)
-xUniform = np.dot(A,uniform)
-xLaplacian = np.dot(A,laplacian)
+def mix(x):
+    A = np.asarray([[4,3],[2,1]])
+    return np.dot(A,x)
 
 # b) center to zero mean 
-xNormalCentered = xNormal - xNormal.mean()
-xUniformCentered = xUniform - xUniform.mean()
-xLaplacianCentered = xLaplacian - xLaplacian.mean()
+def center(x):
+    return x - x.mean(axis=1, keepdims=True)
 
 # c) apply PCA and project data onto PC
-pca = PCA()
-xNormalCenteredProjected = pca.fit_transform(xNormalCentered)
-normalVar = pca.explained_variance_
+def applyPCA(x):
+    pca = PCA()
+    pca.fit(x)
+    return pca.transform(x)
 
-pca = PCA()
-xUniformCenteredProjected = pca.fit_transform(xUniformCentered)
-uniformVar = pca.explained_variance_
-
-pca = PCA()
-xLaplacianCenteredProjected = pca.fit_transform(xLaplacianCentered)
-laplacianVar = pca.explained_variance_
-
-print(xNormalCenteredProjected)
 # d) scale data to unit variance
-scaledNormal = xNormalCenteredProjected / 1
-print(xNormalCentered.shape)
+def scale(x):
+    return pre.maxabs_scale(x)
 
+# e rotate data
+def rotate(x, angle): # x of shape (2,n)
+    R = np.asarray([[math.cos(angle), -math.sin(angle)],[math.sin(angle), math.cos(angle)]])
+    return np.dot(R, x)
 
-##defines
-#samplerate = 8192
-#N = 2
-#p = 18000
-#
-##load the data
-#dataSet1 = np.loadtxt('./sounds/sound1.dat')
-#dataSet2 = np.loadtxt('./sounds/sound2.dat')
-#s = np.stack([dataSet1, dataSet2], axis=0)
-#
-#print (s.shape)
-#
-## create random and invertible NxN (2x2) matrix
-#while True:
-#    A = np.random.rand(2,2)
-#    if np.linalg.det(A) != 0.0:
-#        break
-#print("A=" + str(A))
-#
-##mix the sources
-#x = np.matmul(A, s)
-#print(x[:,:10])
-#print(x.shape)
-#
-## remove temporal structure by permutation 
-#x_per = x[:, np.random.permutation(range(0,18000))]
-#print(x_per[:,:10])
-#print("x_per=" + str(x_per.shape))
-#
-##calculate correlation between sources and mixtures
-#s_std = np.std(s,axis=1)
-#x_per_std = np.std(x_per,axis=1)
-#print(s_std)
-#print(x_per_std) 
-#
-#corr = np.zeros((2, 2))
-#for i in range(0,2):
-#	for j in range(0,2):
-#		#print("i=" + str(i) + " j=" + str(j))
-#		corr[i,j] = np.cov(s[i,:], x_per[j,:])[0][1] / (s_std[i] * x_per_std[j])
-#		#print(np.cov(s[i,:], x_per[j,:]))
-#		#print("corr[i,j]=" + str(corr[i,j]))
-#print("correlations=" + str(corr))
-#
-##center the data
-#x_per = x_per - np.mean(x_per,axis=0);
-#
-##initialize W at random
-#W = np.random.rand(2,2)
-#W_nat = np.copy(W)
-#print("W_nat=" + str(W_nat))
-#def stepSigmoid(y):
-#	return 1 - 2 * (1 / (1 + np.exp(-y)))
-#	
-#vStepSigmoid = np.vectorize(stepSigmoid)
-#eps = 0.01
-#for i in range(0,1):
-#	W_inv = np.linalg.inv(W) 
-##	print("W_inv=" + str(W_inv)) 
-##	currX = x_per[:,i]
-##	print("currX=" + str(currX)) 
-##	inpSig = np.dot(W,x_per[:,i])
-##	print("inpSig=" + str(inpSig)) 
-##	print("inpSigShape=" + str(inpSig.shape)) 
-##	vStepSigmoidRes = vStepSigmoid(np.dot(W,x_per[:,i]))
-##	print("vStepSigmoidRes=" + str(vStepSigmoidRes)) 
-##	print("vStepSigmoidResShape=" + str(vStepSigmoidRes.shape)) 
-##	outer = np.outer(vStepSigmoid(np.dot(W,x_per[:,i])),x_per[:,i])
-##	print("outer=" + str(outer)) 
-##	print("outerShape=" + str(outer.shape)) 
-#	dW = W_inv.T + np.outer(vStepSigmoid(np.dot(W,x_per[:,i])),x_per[:,i])
-##	print("at iter" + str(i) + ", dW=" + str(dW))
-#	W = W + eps * dW
-##	print("at iter" + str(i) + ", W=" + str(W))
-#	#W_nat_inv = np.linalg.inv(W_nat) 
-#	#dW_nat = W_nat_inv.T + np.outer(vStepSigmoid(np.dot(W_nat,x_per[:,i])),x_per[:,i])
-#	#W_nat = W_nat + eps * dW_nat * (np.matmul(W_nat.T, W_nat))
-#
-#print("final=" + str(A - np.linalg.inv(W)))
-##print("final nat=" + str(A - np.linalg.inv(W_nat)))
-#
-#
-#
-#
-#
-#
-#
-#
+def rotateAndGetKurtosis(x, angle):
+    scaledRot = rotate(x.T, angle).T
+    kurt = scipy.stats.kurtosis(scaledRot)    
+    #print("kurt: ", kurt)
+    return kurt
+
+def findMinAndMaxKurt(x, angles):
+    kurts = np.empty((angles.shape[0],2))
+    idx = 0
+    for angle in angles:
+        kurt = rotateAndGetKurtosis(scaledNormal, angle)
+        kurts[idx] = kurt
+        idx += 1
+    
+    maxIdx = np.argmax(kurts[:,0])
+    minIdx = np.argmin(kurts[:,1])
+    return maxIdx, minIdx
+
+def plotdata(source, mixed, centered, projected, scaled, rotMin, rotMax):    
+    # Plotting the results.
+ 
+    df = pd.DataFrame(source, columns=["x", "y"])
+    sea.jointplot(x="x", y="y", data=df)
+    plt.title('Sources')
+    
+    df = pd.DataFrame(mixed, columns=["x", "y"])
+    sea.jointplot(x="x", y="y", data=df)
+    plt.title('Mixed')
+    
+    df = pd.DataFrame(centered, columns=["x", "y"])
+    sea.jointplot(x="x", y="y", data=df)
+    plt.title('Centered')
+    
+    df = pd.DataFrame(projected, columns=["x", "y"])
+    sea.jointplot(x="x", y="y", data=df)
+    plt.title('Projected')
+    
+    df = pd.DataFrame(scaled, columns=["x", "y"])
+    sea.jointplot(x="x", y="y", data=df)
+    plt.title('Scaled')
+    
+    df = pd.DataFrame(rotMin, columns=["x", "y"])
+    sea.jointplot(x="x", y="y", data=df)
+    plt.title('Rotation min kurtosis')
+    
+    df = pd.DataFrame(rotMax, columns=["x", "y"])
+    sea.jointplot(x="x", y="y", data=df)
+    plt.title('Rotation max kurtosis')
+
+def runExercise(x):
+    xNormal = mix(x)
+    xNormalCentered = center(xNormal)
+    # transpose data for further processing
+    xNormalCentered = xNormalCentered.T
+    
+    projectedNormalCentered = applyPCA(xNormalCentered)
+    scaledNormal = scale(projectedNormalCentered)
+    
+    angles = np.arange(0, 2, 1/50)
+    angles = angles * math.pi
+    maxIdx, minIdx = findMinAndMaxKurt(scaledNormal, angles)
+    
+    rotNormMax = rotate(scaledNormal.T, angles[maxIdx])
+    rotNormMin = rotate(scaledNormal.T, angles[minIdx])
+    
+    plotdata(normal.T, xNormal.T, xNormalCentered, projectedNormalCentered, scaledNormal, rotNormMin.T, rotNormMax.T)
+    plt.show()
+
+print("Normal")
+runExercise(normal)
+print("Uniform")
+runExercise(uniform)
+print("Laplacian")
+runExercise(laplacian)
