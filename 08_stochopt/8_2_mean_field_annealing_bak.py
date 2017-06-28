2f98a6bfe8e62c8dd7eb81806ee24c619139c041
Fabian <f.frank@campus.tu-berlin.de>
2017-06-28 21:49:57 +0200

    8.2

--- 08_stochopt/8_2_mean_field_annealing_bak.py
+++ 08_stochopt/8_2_mean_field_annealing_bak.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 numNodes = 6
 
@@ -6,22 +7,18 @@ numNodes = 6
 W = np.random.rand(numNodes,numNodes)
 W = (W + W.T) * 0.5
 W = W - np.diag(W.diagonal())
-#print(W)
-#print(W[0,:])
 
 # 8.2 Initialization
 def init(numNodes):
-    beta = 0.25
-    tau = 1.05
+    beta = 0.5
+    tau = 1.15
     t_max = 20
-    eps = 1e-9
+    eps = 1e-10
     state = 2 * np.random.rand(numNodes) - 1
     meanField = np.zeros([6])
     return beta, tau, t_max, eps, state, meanField
 
 beta, tau, t_max, eps, initstate, meanField = init(numNodes)
-print ("state: ",initstate)
-print("eps: ", eps)
 
 # 8.2 Optimization
 historyBetaEnergy = np.empty((2,0))
@@ -32,56 +29,52 @@ def calcEnergy(state):
 
 def saveBetaAndEnergy(state):
     energy = calcEnergy(state)
-#    print("Energy: ", energy)
-#    print(historyBetaEnergy.shape, np.asarray([beta, energy]).reshape((2,1)).shape)
     global historyBetaEnergy 
     historyBetaEnergy = np.append(historyBetaEnergy, np.asarray([beta, energy]).reshape((2,1)), axis=1)
     
-def computeMeanField(s, i):
-     e = 0
-     for j in range(numNodes):
-         if j != i:
-             e += -W[i,j] * state[j] 
-             #print("e", e)
-    print("e", e)
-    meanField[i] = e
-    #state[i] = np.tanh(np.deg2rad(-beta * e))
-    state[i] = np.tanh(-beta * e)
-            
-    print("meanField: ", meanField)
+def computeMeanField(state):
+    for i in range(numNodes):
+        e = 0
+        for j in range(numNodes):
+            if j != i:
+                e += -W[i,j] * state[j] 
+        meanField[i] = e
+        state[i] = np.tanh(-beta * e)            
     ret = meanField
     return ret, state
 
 def updateState(e):
-    st = np.tanh(np.deg2rad(-beta * e))
-    #st = np.tanh(-beta * e)
-    print("st: ", st, beta, e)
+    st = np.tanh(-beta * e)
     return st
 
 
 for t in range(t_max):
-    state = initstate
+    state = np.copy(initstate)
     e_new = np.zeros((numNodes))
     e_old = np.ones((numNodes)) * eps + 0.1
     counter = 0
     nor = np.linalg.norm(e_old - e_new)
     while (nor > eps ):
-        e_old = e_new
+        e_old = np.copy(e_new)
         e_new, state = computeMeanField(state)
-        print("e_old  / e_new")
-        print(e_old)
-        print(e_new)
-        #state = updateState(e_new)
-        print("State: ", state)
         saveBetaAndEnergy(state)
         counter += 1
         nor = np.linalg.norm(e_old - e_new)
         print("Norm: ", nor)
     beta = beta * tau
-    print("beta: ", beta)
     print("Done! ", counter)
     print("")
     
 print(historyBetaEnergy)
+plt.figure()
+plt.plot(1/historyBetaEnergy[0,:])
+plt.xlabel('Iterations')
+plt.ylabel('Temperature')
 
-#plotData()
+plt.figure()
+plt.plot(historyBetaEnergy[1,:])
+plt.xlabel('Iterations')
+plt.ylabel('Energy')
+
+plt.show()
+
